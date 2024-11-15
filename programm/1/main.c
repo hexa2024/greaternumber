252c090c273847dd52f2e18aaa9d739def8ee073
Prema <premaranidas06@gmail.com>
2024-11-15 18:38:14 +0600

    Add files via upload

---
+++ programm/1/main.c
@@ -0,0 +1,13 @@
+#include<stdbool.h>
+#include<stdio.h>
+int main()
+{
+// Create boolean variables
+    bool isProgrammingFun = true;
+    bool isFishTasty = false;
+
+// Return boolean values
+    printf("%d\n", isProgrammingFun);   // Returns 1 (true)
+    printf("%d", isFishTasty);        // Returns 0 (false)
+}
+
